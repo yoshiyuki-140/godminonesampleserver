16d8f71a18eb7d73fa4d92c24bc3d653c8a4c180
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-04 15:45:23 +0900

    add : アカウント登録機能のロジックを追加

---
+++ cmd/account/register.go
@@ -0,0 +1,21 @@
+package account
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yoshiyuki-140/godminonesampleserver/models"
+	"gorm.io/gorm"
+)
+
+func Register(c *gin.Context, db *gorm.DB) {
+	var user models.User
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+	db.Create(&user)
+
+}
