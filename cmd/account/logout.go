e85b98923ea510df47e8d76a59c9c47a83840113
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-04 15:44:47 +0900

    add : ログアウトのロジックを実装

---
+++ cmd/account/logout.go
@@ -0,0 +1,28 @@
+package account
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/yoshiyuki-140/godminonesampleserver/cmd/utils"
+	"github.com/yoshiyuki-140/godminonesampleserver/models"
+	"gorm.io/gorm"
+)
+
+func Logout(c *gin.Context, db *gorm.DB) {
+	// セッションIDからユーザーを取得
+	var user models.User
+	err := utils.GetUserBySessionID(c, db, &user)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// そのユーザーの持つセッションIDを0にリセットする
+	log.Println("user:", user)
+	user.SessionId = 0
+	db.Save(&user)
+	c.JSON(http.StatusOK, gin.H{"message": "Success", "msg": user.ID})
+	return
+}
